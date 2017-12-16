e36b69d5baf37203db305d1d1592b06f73be5f17
daniyalnagori <daniyalnagori@yahoo.com>
2017-12-16 13:54:06 +0500

    fewchanges

--- src/router.js
+++ src/router.js
@@ -5,12 +5,11 @@ import PreviousPolls from './components/previous-polls';
 import ShowPollQuestion from './components/show-poll-question';
 import PollList from './components/poll-list';
 import Example from './components/graph';
-import CreatePollImg from './images/createPoll.jpg';
 
 
 
 const Main = () => (
-    <main style={{background:`url(${CreatePollImg})`,backgroundRepeat:'no-repeat'}}>
+    <main>
         <Switch>
             <Route exact path='/' component={CreatePoll}/>
             <Route path='/previouspoll' component={PreviousPolls}/>
